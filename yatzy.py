1a4fc7a6e8d86619abe3817e17415a5d87689976
Snarkjakten <42779066+Snarkjakten@users.noreply.github.com>
2018-08-29 10:21:08 +0200

    Update yatzy.py
    från dagens labb

--- yatzy.py
+++ yatzy.py
@@ -1,54 +1,73 @@
 # python 3.6
 import random
 import numpy
+s = 0
+tur = 1
 while True:
 
-    """ skapar matris som ska bli scoreboard for s antal spelare"""
-    s = int(input("Hur manga spelare spelar?"))
-    numpy.set_printoptions(threshold=numpy.nan)
-    scoreboard = numpy.zeros((7, s+1))
+    if s < 1:
+        """ skapar matris som ska bli scoreboard for s antal spelare"""
+        print("Nytt spel")
+        s = int(input("Hur manga spelare spelar?"))
+        numpy.set_printoptions(threshold=numpy.nan)
+        scoreboard = numpy.zeros((7, s+1))
 
-    """ fixar poangkategorier i scoreboard """
-    x = 7
-    for x in range(7):
-        scoreboard[x, 0] = x
-        x += 1
-        
-    """" fixar spelarkolumnen i scoreboard """
-    x = 1
-    for x in range(s+1):
-        scoreboard[0, x] = x
-        x += 1
-    print(scoreboard)
-    
-    """ kastar n tarningar som sparas i en lista """
-    dielist = []
-    x = 0
-    n = int(input("hur manga tarningar vill du kasta?"))
-    for x in range(n):  # x < n
-        kast = random.randint(1,6)
-        dielist.append(kast)
-    print(dielist)
+        """ fixar poangkategorier i scoreboard """
+        x = 7
+        for x in range(7):
+            scoreboard[x, 0] = x
+            x += 1
 
-    """ kastar om utvalda tarningar, 2 ggr, uppdaterar listan """
-    x = 0
-    for x in range(2):
-        omkast = input("vilka tarningar vill du kasta om?")
-        omkast = omkast.split()
-        k = 0
-        for x in range(len(omkast)):
-            dielist[int(omkast[k])-1] = random.randint(1, 6)
-            k += 1
+        """" fixar spelarkolumnen i scoreboard """
+        x = 1
+        for x in range(s+1):
+            scoreboard[0, x] = x
+            x += 1
+        print(scoreboard)
+
+    def kast(tur):
+
+        """ kastar n tarningar som sparas i en lista """
+        dielist = []
+        x = 0
+        n = int(input("hur manga tarningar vill du kasta?"))
+        for x in range(n):  # x < n
+            kast = random.randint(1,6)
+            dielist.append(kast)
         print(dielist)
-        x += 1
 
-    """ valjer vilka siffror man vill spara och summerar dem """
-    spara = int(input("vad vill du spara?"))
-    x = 0
-    count = 0
-    for x in range(len(dielist)):
-        if dielist[x] == spara:
-            count += 1
-    print("Summan ar", count*spara)
+        """ kastar om utvalda tarningar, 2 ggr, uppdaterar listan """
+        x = 0
+        for x in range(2):
+            omkast = input("vilka tarningar vill du kasta om?")
+            omkast = omkast.split()
+            k = 0
+            for x in range(len(omkast)):
+                dielist[int(omkast[k])-1] = random.randint(1, 6)
+                k += 1
+            print(dielist)
+            x += 1
 
-    print("Nasta spelares tur")
+        """ valjer vilka siffror man vill spara och summerar dem """
+        spara = int(input("vad vill du spara?"))
+        x = 0
+        count = 0
+        for x in range(len(dielist)):
+            if dielist[x] == spara:
+                count += 1
+        print("Summan ar", count*spara)
+
+        scoreboard[spara, tur] = count*spara
+
+        print(scoreboard)
+        return scoreboard
+    y = 0
+    for y in range(6):
+        tur = 1
+        x = 0
+        for x in range(s):
+            scoreboard = kast(tur)
+            tur += 1
+            print("Nasta spelares tur")
+    s = 0
+    print("spelet r slut")
